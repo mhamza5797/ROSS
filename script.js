2e8e8732aa5403366d554ef73241e886533a8215
Muhammad Hamza <55306965+mhamza5797@users.noreply.github.com>
2025-04-24 23:15:39 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ document.addEventListener('DOMContentLoaded', function () {
       let flatRate = 0;
 
       if (addSqft <= 425) {
-        flatRate = 180;         
+        flatRate = 60;         
         perRoomRate = 55;
       } else if (addSqft >= 425 && addSqft <= 545) {
         flatRate = 180;
